cfa4f2bc882b26ae014527d0c28d7941913be8e1
caio pereira <caiomp87@gmail.com>
2024-01-04 12:26:07 -0300

    implements update and delete meal

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -40,14 +40,6 @@ export async function mealsRoute(app: FastifyInstance) {
     },
   )
 
-  app.get(
-    '/metrics',
-    { preHandler: [checkSessionIdExists] },
-    async (request, reply) => {
-      return reply.status(200).send()
-    },
-  )
-
   app.post(
     '/',
     { preHandler: [checkSessionIdExists] },
@@ -80,6 +72,41 @@ export async function mealsRoute(app: FastifyInstance) {
     '/:id',
     { preHandler: [checkSessionIdExists] },
     async (request, reply) => {
+      const updateMealParamSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = updateMealParamSchema.parse(request.params)
+
+      const updateMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      })
+
+      const { name, description, isOnDiet, date } = updateMealBodySchema.parse(
+        request.body,
+      )
+
+      const filter = {
+        user_id: request.user?.id,
+        id,
+      }
+
+      const meal = await knex('meals').where(filter).first()
+
+      if (!meal) {
+        return reply.status(404).send({ error: 'meal not found' })
+      }
+
+      await knex('meals').where(filter).update({
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.getTime(),
+      })
+
       return reply.status(204).send()
     },
   )
@@ -88,7 +115,34 @@ export async function mealsRoute(app: FastifyInstance) {
     '/:id',
     { preHandler: [checkSessionIdExists] },
     async (request, reply) => {
+      const deleteMealParamSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = deleteMealParamSchema.parse(request.params)
+
+      const filter = {
+        user_id: request.user?.id,
+        id,
+      }
+
+      const meal = await knex('meals').where(filter).first()
+
+      if (!meal) {
+        return reply.status(404).send({ error: 'meal not found' })
+      }
+
+      await knex('meals').where(filter).delete()
+
       return reply.status(204).send()
+    },
+  )
+
+  app.get(
+    '/metrics',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      return reply.status(200).send()
     },
   )
 }
