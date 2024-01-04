5a3b7456426273228422bddd56ff98027acd9cf1
caio pereira <caiomp87@gmail.com>
2024-01-04 12:52:49 -0300

    implements the user meals metrics

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -142,7 +142,51 @@ export async function mealsRoute(app: FastifyInstance) {
     '/metrics',
     { preHandler: [checkSessionIdExists] },
     async (request, reply) => {
-      return reply.status(200).send()
+      const totalMeals = await knex('meals')
+        .where({
+          user_id: request.user?.id,
+        })
+        .orderBy('date', 'desc')
+
+      const totalMealsOnDiet = await knex('meals')
+        .where({
+          user_id: request.user?.id,
+          is_on_diet: true,
+        })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalMealsOffDiet = await knex('meals')
+        .where({
+          user_id: request.user?.id,
+          is_on_diet: false,
+        })
+        .count('id', { as: 'total' })
+        .first()
+
+      const { bestOnDietSequence } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.is_on_diet) {
+            acc.currentSequence += 1
+          } else {
+            acc.currentSequence = 0
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence
+          }
+
+          return acc
+        },
+        { bestOnDietSequence: 0, currentSequence: 0 },
+      )
+
+      return reply.status(200).send({
+        totalMeals: totalMeals.length,
+        totalMealsOnDiet: totalMealsOnDiet?.total,
+        totalMealsOffDiet: totalMealsOffDiet?.total,
+        bestOnDietSequence,
+      })
     },
   )
 }
